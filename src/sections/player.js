8735bcefb82b9e4c3a2c310e9cb2e9b3987623b2
Pablo Crotti <pablo.crotti@heig-vd.ch>
2023-03-31 16:14:29 +0200

    Manifest

--- src/sections/player.js
+++ src/sections/player.js
@@ -25,7 +25,6 @@ const songListContainer = document.querySelector('.songs')
 const playFunc = (song, songs) => {
   currentSong = song
   playerPlayIcon.classList.remove('material-icons-pause')
-  console.log(currentSong)
     audioPlayer.src = currentSong.url
     audioPlayer.play()
 
